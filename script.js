ccdf5a0a55925b61ac408c7a80155bb4a2a2e684
Geordie King <geordie.king@gmail.com>
2021-09-11 18:10:42 +0100

    scaffold basic js and class

---
+++ script.js
@@ -0,0 +1,36 @@
+class Calculator {
+  constructor (txtPrev, txtCurr) {
+    this.txtPrev = txtPrev
+    this.txtCurr = txtCurr
+  }
+
+  clear() {
+
+  }
+  delete() {
+
+  }
+  divide() {
+
+  }
+  multiply(){
+
+  }
+  add(){
+
+  }
+  subtract(){
+    
+  }
+}
+
+const btnNums = document.querySelectorAll('[data-num]')
+const btnOps  = document.querySelectorAll('[data-op]')
+const btnEq = document.querySelector('[data-eq]')
+const btnDel = document.querySelector('[data-del]')
+const btnAC = document.querySelector('[data-ac]')
+const txtPrev = document.querySelector('[data-prev]')
+const txtCurr = document.querySelector('[data-curr]')
+
+
+
